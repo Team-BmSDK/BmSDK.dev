eef5408a1d154c4f618734f8ec2f1fd1c996d785
etkramer <elikramergm@gmail.com>
2026-07-12 09:23:40 -0700

    chore: update site tagline/description

--- src/data/site.ts
+++ src/data/site.ts
@@ -3,9 +3,9 @@ const knight = 'https://github.com/Team-BmSDK/BmSDK-AK'
 
 export const site = {
   name: 'BmSDK',
-  tagline: 'Script the Arkham games in C#',
+  tagline: 'Arkham modding SDK',
   description:
-    'BmSDK is a scripting platform for Batman: Arkham City and Arkham Knight. Extend the game with custom gameplay and logic using a full C# SDK.',
+    'BmSDK is a C#-based script hook/modding SDK for Batman: Arkham City and Arkham Knight. Extend the game with custom gameplay and logic using a full C# SDK.',
 }
 
 export const links = {
